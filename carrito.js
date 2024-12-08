5e49d9212f4985712e3410d2e36c52660ca3daa1
Ayuik <marcosayelen@gmail.com>
2024-12-08 11:43:38 +0100

    comment modified

--- carrito.js
+++ carrito.js
@@ -1,4 +1,4 @@
-//1.Crear función para activar el boton de "Add to cart" si un boton de color esta ":focus"
+//1.Crear función para activar el boton de "Add to cart" si un boton de color esta "clickeado"
 //2.Crear funcion en que al clickear en "add to cart" devuelva "1" (contador)
 //3. Hacer que el "1" aparezca sobre el ícono de la nav bar
 
@@ -17,7 +17,7 @@ for (let i = 0; i < buttons.length; i++) {
 }
 
 
-// 2 
+// 2  y 3
  let counter = 0
  function contador() {
     counter += 1;
